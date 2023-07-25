2af25aadc5e016fa0e69bee49dacf94a7f1a946f
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-07-25 19:11:40 -0400

    updated the code and commented out the code

--- routes/routes.js
+++ routes/routes.js
@@ -10,5 +10,20 @@ router.get('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
+// posts the stored data to the notes.htmls
+router.post('/notes', (req, res) => {
+    store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => console.log(err));
+});
+
+// removes the notes by id from the db.json
+router.delete("/notes/:id", (req, res) => {
+    store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
