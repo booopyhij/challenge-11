0c5216d0c8fa5ccce096a5e6c9ab345eee96c11d
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-09-23 15:28:39 -0400

    updated and fixed a bug error that prevented live server opening

--- db/store.js
+++ db/store.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const uuidv1 = require('uuid/v1');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+// const { getNotes, addNote, removeNote} = require('../public/js/index.js')
 
 //class to store get and remove notes as needed
 class Store {
